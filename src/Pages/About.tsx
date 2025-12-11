bcaed334ce1ff31fc0e12dba03ddf4ae74249474
karis-cod <chukwuebukaogu3@gmail.com>
2025-12-11 10:52:27 -0800

    fixing image bug--2

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -21,7 +21,7 @@ function AboutPage() {
       <p className='grid-1-p' >With over 6 months of experience in web development, I specialize in creating responsive, accesible and performantweb applications using morden technologies.</p>
       <p className='grid-1-p2'>I'm passionate about creating elegant solutions to complex problems, and i'm constantly learning new technologies and techniques to stay at the forefront of the evr-elvolving web landscape.</p>
       <button className="about-btn"><a className="aa-b" href="#contact">Get in Touch</a></button>
-      <button className="about-btn">Download CV</button>
+      <button className="about-btn"><a className="aa-b" href="resume.docx" download rel="noopener noreferrer" target="blank">Download resume</a></button>
     </div>
     <div className='grid-2'>
       <div data-aos="fade-down-left" className='grid-2-con'>
